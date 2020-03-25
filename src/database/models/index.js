ad95403cebe768ef05a2e5ffeb7f85c79980a4db
usefss <flasnm@yahoo.com>
2020-03-25 21:02:41 +0430

    register user test

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -80,7 +80,7 @@ Object.keys(db).forEach(modelName => {
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-console.log('index.js had been called')
+console.log('index.js had been called ')
 // console.log(db)
 module.exports = db;
 
--- src/logic/bot.js
+++ src/logic/bot.js
@@ -1,12 +1,12 @@
 const Telegraf = require('telegraf')
 
 const session = require('telegraf/session')
-const Statge = require('telegraf/stage')
+const Stage = require('telegraf/stage')
 const Scene = require('telegraf/scenes/base')
 const { leave } = Stage
 
 const { db } = require('../database/models/index.js');
-
+console.log(db)
 
 class Bot {
     constructor(token) {
@@ -16,14 +16,54 @@ class Bot {
             console.error(`Bot error: ${error}`);
         });
     }
-
     async init() {
+      this.bot.command('all', (ctx) => {
+        console.log('db is : '+ this.database)
+        this.database['User'].findAll()
+        .then((users) => {
+          // JSON.stringify(users, null, 4)
+          ctx.reply('ehhh')
+        })
+      })
       this.bot.command('register', (ctx) => ctx.scene.enter('register'))
-
+      const register = new Scene('register')
+      register.enter((ctx) => {
+        ctx.reply('hi please enter your age: ')
+        var tempUser = {}
+      })
+      register.leave((ctx) => {
+        this.database['User'].create({
+            name: tempUser['name'],
+            chatId: tempUser['chatId'],
+            class: tempUser['class'],
+            age: tmepUser['age'],
+            province: tempUser['province']
+        })
+        ctx.reply('thank you bye')
+      })
+      register.on('text', (ctx) => {
+          if (!tempUser['age']){
+            tempUser['age'] = ctx['text']
+            tempUser['chatId'] = ctx['chat'].id
+            tempUser['name'] = ctx['message']['from'].first_name
+            tempUser['class'] = 'normal'
+            ctx.reply('lets enter your province')
+          }
+          else{
+            tempUser['province'] = ctx['text']
+            leave()
+          }
+      })
+      const stage = new Stage()
+      stage.command('cancel', leave())
+      stage.register(register)
+      this.bot.use(session())
+      this.bot.use(Telegraf.log())
+      this.bot.use(stage.middleware())
     }
 
     start() {
-        this.bot.startPolling();
+        this.bot.launch();
     }
 }
 
