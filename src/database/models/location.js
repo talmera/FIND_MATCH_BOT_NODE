c2f89068d81e89c002a26d7f50e030e2022036de
usefss <flasnm@yahoo.com>
2020-03-26 17:43:34 +0430

    location text allow null

--- src/database/models/location.js
+++ src/database/models/location.js
@@ -1,7 +1,10 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Location = sequelize.define('Location', {
-    comment: DataTypes.TEXT
+    comment: {
+      DataTypes.TEXT,
+      allowNull: true
+    }
   }, {});
   Location.associate = function(models) {
     // associations can be defined here
